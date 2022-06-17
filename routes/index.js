64666ffb72a95438b7ee6505d110d589d93bba82
Salverte <silvina.alverte@gmail.com>
2022-06-16 21:23:58 -0300

    partials, routes, CSS

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Splinter' });
 });
 
+/* GET Nosotros. */
+router.get('/nosotros', function(req, res, next) {
+  res.render('pages/nosotros');
+});
+
+/* GET Contacto */
+router.get('/contacto', function(req, res, next) {
+  res.render('pages/contacto');
+});
 module.exports = router;
